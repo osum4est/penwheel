2fcf9c80e68692310289ce25b7f377c5bc1267b7
Forrest Jones <fjones-dev@8bitforest.com>
2021-11-03 22:15:05 -0600

    :sparkles: Hot reload config file

--- pen_wheel/pen_wheel.cpp
+++ pen_wheel/pen_wheel.cpp
@@ -5,15 +5,27 @@ pen_wheel *pen_wheel::_instance = nullptr;
 void pen_wheel::start() {
     _instance = this;
 
-    std::filesystem::path config_file = "config.yaml";
-    if (std::filesystem::is_regular_file(config_file))
-        _config = std::make_unique<pen_wheel_config>("config.yaml");
-    else
-        _config = std::make_unique<pen_wheel_config>();
+    _config_path = "config.yaml";
+
+    load_config();
+    _config_watcher.addPath(QString::fromStdString(_config_path));
+
+    connect(&_config_watcher, &QFileSystemWatcher::fileChanged, this, &pen_wheel::config_changed);
 
     _overlay = std::make_unique<pen_wheel_window>();
 }
 
 const pen_wheel_config *pen_wheel::config() {
     return _instance->_config.get();
+}
+
+void pen_wheel::config_changed(const QString &path) {
+    load_config();
+}
+
+void pen_wheel::load_config() {
+    if (std::filesystem::is_regular_file(_config_path))
+        _config = std::make_unique<pen_wheel_config>(_config_path);
+    else
+        _config = std::make_unique<pen_wheel_config>();
 }
--- pen_wheel/pen_wheel.h
+++ pen_wheel/pen_wheel.h
@@ -3,17 +3,24 @@
 
 #include <config/pen_wheel_config.h>
 #include <windows/pen_wheel_window.h>
+#include <QFileSystemWatcher>
 
-class pen_wheel {
+class pen_wheel : public QObject {
     static pen_wheel *_instance;
 
-    std::unique_ptr<pen_wheel_window> _overlay;
+    std::filesystem::path _config_path;
+    QFileSystemWatcher _config_watcher;
     std::unique_ptr<pen_wheel_config> _config;
+    std::unique_ptr<pen_wheel_window> _overlay;
 
 public:
     void start();
 
     static const pen_wheel_config *config();
+
+private:
+    void config_changed(const QString &path);
+    void load_config();
 };
 
 #endif
